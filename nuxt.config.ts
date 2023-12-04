e02e34d522e962c5da5e107bf63df906f86827c3
h-mitou <h-mitou@gajumale.co.jp>
2023-12-04 11:34:38 +0900

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,25 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  srcDir: "src/",
+  nitro: {
+    preset: "aws-lambda",
+    esbuild: {
+      options: {
+        target: "esnext",
+      },
+    },
+  },
+  vite: {
+    build: {
+      target: "esnext",
+    },
+  },
+  runtimeConfig: {
+    tableName: "simple-bbs",
+    awsEndpoint: "",
+    awsSecretAccessKey: "",
+    awsAccessKeyId: "",
+    awsDefaultRegion: "",
+  },
+});
